6d34441af4443a8460d3d17eee573aa4c301c646
nazlicansemiz <nazlicankucuksabanoglu@gmail.com>
2025-04-21 00:11:39 +0100

    Update Driver.java to use WebDriverManager
    - Removed manual chromedriver path setup
    - Added WebDriverManager to automatically manage driver versions
    - Improves cross-platform compatibility (Mac/Windows/Linux)s

--- src/test/java/com/amazon/base/Driver.java
+++ src/test/java/com/amazon/base/Driver.java
@@ -1,6 +1,7 @@
 package com.amazon.base;
 
 import com.amazon.utilities.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -22,6 +23,7 @@ public class Driver {
 
             switch (browser) {
                 case "chrome":
+                    WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--start-maximized");
                     chromeOptions.addArguments("--disable-notifications");
