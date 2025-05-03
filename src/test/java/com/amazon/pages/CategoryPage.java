47ab052e423cc7a520eff146f1679deecb531a2f
M.Merve KOŞAR <merveabd.61@gmail.com>
2025-05-03 17:39:17 +0300

    Category Feature updated

--- src/test/java/com/amazon/pages/CategoryPage.java
+++ src/test/java/com/amazon/pages/CategoryPage.java
@@ -2,16 +2,10 @@ package com.amazon.pages;
 
 
 import com.amazon.utilities.WebDriverUtils;
-import io.cucumber.plugin.event.Node;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.List;
-
 
 import static com.amazon.stepdefinitions.Hooks.driver;
 
@@ -34,11 +28,13 @@ public class CategoryPage {
     public List<WebElement> shopByDepartmentVisibleSubHeadsList;
 
     @FindBy(xpath = "//a[@data-menu-id='28' or  @data-menu-id='29' or @data-menu-id='30' ]")
-    private List<WebElement> programsFeaturesSubHeadsList;
+    public List<WebElement> programsFeaturesVisibleSubHeadsList;
 
-    @FindBy(xpath = "((//ul[@data-menu-id='1']//div[@role='heading'])[4])/parent::li/following-sibling::li")
-    private List<WebElement> helpAndSettingsSubHeadsList;
+    @FindBy(xpath = "((//ul[@data-menu-id='1']//div[@role='heading'])[4])/parent::li/following-sibling::li//a")
+    public List<WebElement> helpAndSettingsSubHeadsList;
 
+    @FindBy(xpath = "//a[text()='Shop By Interest']")
+    public WebElement shopByInterest;
 
 
     // Digital Content & Devices , Shop by Department , Programs & Features , Help & Settings
@@ -77,12 +73,53 @@ public class CategoryPage {
     }
 
     // Electronics , Computers , Smart Home , Arts & Crafts
-    // The method of accessing the subheadings of Digital Content & Devices with text as a web element
-    public WebElement ShopByDepartmentVisibleSubCategories(String subcategoryText) {
-        WebElement subcategory = null;
+    // The method of accessing the subheadings of Shop By Departments with text as a web element
+    public WebElement shopByDepartmentVisibleSubCategories(String subcategoryText) {
+        WebElement visibleSubcategory = null;
         // TODO --> change Explicit wait
         WebDriverUtils.wait(2);
         for (WebElement w : shopByDepartmentVisibleSubHeadsList) {
+            if (w.getText().equals(subcategoryText)) {
+                visibleSubcategory = w;
+                //TODO --> USE LOGGING
+                System.out.println(w.getText());
+                break;
+            }
+        }
+        return visibleSubcategory;
+    }
+
+
+    // Gift Cards , Shop By Interest , Amazon Live , International Shopping
+    // The method of accessing the subheadings of Programs & Features with text as a web element
+    public WebElement programAndFeatureVisibleSubCategories(String subcategoryText) {
+        WebElement visibleSubcategory = null;
+        // TODO --> change Explicit wait
+        WebDriverUtils.wait(2);
+        for (WebElement w : programsFeaturesVisibleSubHeadsList) {
+            if (subcategoryText.equals(shopByInterest.getText())) {
+                visibleSubcategory = w;
+                //TODO --> USE LOGGING
+                System.out.println(w.getText());
+                break;
+            } else if (w.getText().equals(subcategoryText)) {
+                visibleSubcategory = w;
+                //TODO --> USE LOGGING
+                System.out.println(w.getText());
+                break;
+            }
+        }
+        return visibleSubcategory;
+    }
+
+
+    // Your Account , English , United States , Customer Service , Sign in
+    // The method of accessing the subheadings of Helps & Settings with text as a web element
+    public WebElement helpAndSettingSubCategories(String subcategoryText) {
+        WebElement subcategory = null;
+        // TODO --> change Explicit wait
+        WebDriverUtils.wait(5);
+        for (WebElement w : helpAndSettingsSubHeadsList) {
             if (w.getText().equals(subcategoryText)) {
                 subcategory = w;
                 //TODO --> USE LOGGING
@@ -92,8 +129,4 @@ public class CategoryPage {
         }
         return subcategory;
     }
-
-
-
-
 }
